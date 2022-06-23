8a24ddca5a9d08de09eac11f2fc3912656c15546
Kalessia <73894713+Kalessia@users.noreply.github.com>
2022-06-23 12:08:00 +0200

    Delete kilolib directory

--- kilolib/message_send.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MESSAGE_SEND_H__
-#define __MESSAGE_SEND_H__
-
-#include "message.h"
-
-extern volatile uint8_t tx_mask;
-uint8_t message_send(const message_t *);
-
-#endif//__MESSAGE_SEND_H__
